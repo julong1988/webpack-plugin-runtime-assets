8140fdfca865fb5055512640353f1dca65ea8f89
JuLong <julong1988@naver.com>
2021-07-04 15:32:56 +0900

    fix(common): 불필요한 파일 삭제

--- scripts/build.js
+++
@@ -1,8 +0,0 @@
-const Listr = require('listr');
-const { buildAllTasks } = require('./tasks');
-
-const tasks = new Listr(buildAllTasks);
-
-tasks.run().then(() => console.log('Success')).catch(err => {
-	console.error(err);
-});
--- scripts/prepublish.js
+++
@@ -1,9 +0,0 @@
-const Listr = require('listr');
-
-const { buildAllTasks, versionUpdateTasks } = require('./tasks');
-
-const tasks = new Listr([...buildAllTasks, ...versionUpdateTasks]);
-
-tasks.run().then(() => console.log('Success')).catch(err => {
-	console.error(err);
-});
