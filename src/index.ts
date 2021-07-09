e95b1ac69819071373fc906b18154aa65ebf0721
JuLong <julong1988@naver.com>
2021-07-09 15:05:26 +0900

    fix(common): rome툴 적용

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,53 @@
+import { Compilation, Compiler } from "webpack";
+
 interface Iassets {
-  js: string[];
-  css: string[];
-  other: string[];
+	js: Array<string>;
+	css: Array<string>;
+	other: Array<string>;
 }
 class webpackPluginRuntimeAssets {
-  apply (compiler: any) {
-    compiler.hooks.emit.tap(
-      'webpackPluginRuntimeAssets',
-      (stats: any) => {
-        const assets: Iassets = {
-          js: [],
-          css: [],
-          other: [],
-        };
-        let runtimeChunks: any = [];
-        for (const [, { files }] of stats.namedChunks) {
-          runtimeChunks = [...runtimeChunks, ...files]
-        }
+	apply(compiler: Compiler) {
+		compiler.hooks.emit.tap(
+			"webpackPluginRuntimeAssets",
+			(stats) => {
+				const assets: Iassets = {
+					js: [],
+					css: [],
+					other: [],
+				};
+				let runtimeChunks: Array<string> = [];
+				for (const [, {files}] of stats.namedChunks) {
+					runtimeChunks = [...runtimeChunks, ...files];
+				}
 
-        runtimeChunks.forEach((e: any) => {
-          assets[defineFileExtension(e)].push(stats.outputOptions.publicPath + e);
-        });
+				runtimeChunks.forEach((e) => {
+					assets[defineFileExtension(e)].push(
+						stats.outputOptions.publicPath + e,
+					);
+				});
 
-        // generator json file
-        const assetsData = JSON.stringify(assets, null, 2)
-        stats.assets[`bundle.json`] = {
-          source: () => assetsData,
-          size: () => assetsData.length
-        }
-      }
-    );
-  }
+				// generator json file
+				const assetsData = JSON.stringify(assets, null, 2);
+				// @ts-ignore: Unreachable code error
+				stats.assets["bundle.json"] = {
+					source: () => assetsData,
+					size: () => assetsData.length,
+				};
+			},
+		);
+	}
 }
 
-function defineFileExtension (filename: string): keyof Iassets {
-  const _fileExt = filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+function defineFileExtension(filename: string): keyof Iassets {
+	const _fileExt = filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
 
-  if (_fileExt.startsWith('js')) {
-    return 'js'
-  }
-  if (_fileExt.startsWith('css')) {
-    return 'css'
-  }
-  return 'other'
+	if (_fileExt.startsWith("js")) {
+		return "js";
+	}
+	if (_fileExt.startsWith("css")) {
+		return "css";
+	}
+	return "other";
 }
 
 export default webpackPluginRuntimeAssets;
